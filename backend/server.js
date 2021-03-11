68f2e4d0fe6f9698b9abe26685f50b18870c60a4
Sanchit Hajela <sanchit10hajela@gmail.com>
2021-03-12 00:33:06 +0530

    All added

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const error = require('./middlewares/errorMiddlewareHandler');
+const usersRoute = require('./routes/usersRoute');
+const bookRouter = require('./routes/bookRoutes');
+dotenv.config();
+require('./config/dbConnect')();
+
+const app = express();
+
+//Passing body data
+app.use(express.json());
+
+//Routes
+//Users
+app.use('/api/users', usersRoute);
+//Books
+app.use('/api/books', bookRouter);
+
+//Error middleware
+app.use(error.errorMiddlewareHandler);
+
+//Server
+const PORT = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV=='production'){
+    app.use(express.static('frontend/build'));
+
+}
+app.listen(PORT, () => {
+  console.log(`Server is up and runing ${PORT}`);
+
+
+});
